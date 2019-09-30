872713a0189d6f7072fda6fae07eadad20727186
Pablo Samano <psamano25@alumnos.uaq.mx>
2019-09-30 18:48:37 -0500

    Update urls.py

--- docente/urls.py
+++ docente/urls.py
@@ -2,4 +2,13 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('addMemberToTeam/',views.addMemberToTeam,name='addMemberToTeam'),
+    path('deleteTeamMember/',views.deleteTeamMember,name='deleteTeamMember'),
+    path('addMemberToWs/',views.addMemberToWs,name='addMemberToWs'),
+    path('deleteWsMember/',views.deleteWsMember,name='deleteWsMember'),
+    path('updateWorkshop/',views.updateWorkshop,name='updateWorkshop'),
+    path('updateTeam/',views.updateTeam,name='updateTeam'),
+    path('callTheRoll/',views.callTheRoll,name='callTheRoll'),
+    path('absolveWs/',views.absolveWs,name='absolveWs'),
+    path('statisticsAttendance/',views.statisticsAttendance,name='statisticsAttendance'),
+    path('statisticsMatches/',views.statisticsMatches,name='statisticsMatches'),
 ]
