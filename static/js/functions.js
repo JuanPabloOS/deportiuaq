e4424bf2fb396f97327df5f5dbf59eff2182cf95
Roberto <robert-art1@hotmail.com>
2019-10-27 18:19:42 -0600

    reestructuración del menu

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,3 +1,17 @@
+var coll = document.getElementsByClassName("option");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("option-active-list");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+	  content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
 var launch_toast = ()=>{
 	var x = document.getElementById("toast");
 	console.log("====");
