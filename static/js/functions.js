c7f1df8788daccc0f5e2d22b1b80e92f4e76f4e9
Roberto <robert-art1@hotmail.com>
2019-12-01 10:02:55 -0600

    Avance toast

--- static/js/functions.js
+++ static/js/functions.js
@@ -13,14 +13,12 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 function launch_toast(e){
+  console.log("entrooooooooooooooooooooooooooooo");
 	var x = document.getElementById("toast");
-	console.log("====");
+	
 	console.log(x);
   x.className = "show";
   document.getElementById('desc').innerHTML = e;
 	setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
-}
-function hi(){
-	alert("adad");
 }
 
