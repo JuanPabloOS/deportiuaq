9bddf31812088d0eedea4cb99fe12d666ee1fcc4
JuanPabloOS <jp7patrick@gmail.com>
2019-12-04 14:32:11 -0600

    Toast error & success

--- static/js/toast.js
+++ static/js/toast.js
@@ -1,9 +1,18 @@
-function launch_toast(e){
+function launch_toast(e, type){
     console.log("entrooooooooooooooooooooooooooooo");
-      var x = document.getElementById("toast");
-      
-      console.log(x);
+    var x = document.getElementById("toast");
+    // console.log(x);
+    var icon = document.getElementById("imgToast");
+    var iTag = document.createElement("i");
+    if(type==1){
+      iTag.setAttribute("class","fas fa-check");
+    }else{
+      iTag.setAttribute("class","fas fa-exclamation");
+    }
+    icon.appendChild(iTag);
     x.className = "show";
     document.getElementById('desc').innerHTML = e;
-      setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
   }
+
+
