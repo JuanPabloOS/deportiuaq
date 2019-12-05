d9b865ed742c90de494b4ca29e87941d8d5eaaad
JuanPabloOS <JuanPabloOS@4261811f904c>
2019-12-05 04:21:32 +0000

    Cambios producción

---
+++ collectedstatic/js/functions.js
@@ -0,0 +1,16 @@
+var coll = document.getElementsByClassName("option");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("option-active-list");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+	  content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
+
